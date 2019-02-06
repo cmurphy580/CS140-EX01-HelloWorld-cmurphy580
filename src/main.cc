e0318d4692640967473951e342b1912ca40183d4
conormurphy <cmurphy580@gmail.com>
2019-02-05 20:27:11 -0800

    commit main hello world

---
+++ src/main.cc
@@ -0,0 +1,17 @@
+/**
+ * CS 140 Object-Oriented Programming in C++
+ * Assignment: EX01-HelloWorld
+ *
+ * Statement of code ownership: I hereby state that I have written all of this
+ * code and I have not copied this code from any other person or source.
+ *
+ * @author [Conor Murphy]
+ */
+
+#include <iostream>
+#include <cs140>
+
+int main(int argc, char *argv[]) {
+    std::cout << "Hello, World!" << std::endl;
+    return EXIT_SUCCESS;
+}
